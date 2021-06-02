2b85f8341916aeff415db33adabbe616693b5291
Maxim Kuznetsov <maximxlssoff@gmail.com>
2021-06-02 13:06:35 +0300

    added custom grammar

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import argh
 from argh import arg
 from interpreter import Interpreter
-from grammar import grammar, required_words_in_varnames
 from lark import Lark
+import importlib
 
-def main(source: "Path to the source code file to execute", legacy_vars: "Disable LongDick varible name restrictions"=False):
+def main(source: "Path to the source code file to execute", grammar_file: "Custom grammar file without .py"="grammar", legacy_vars: "Disable LongDick varible name restrictions"=False):
     with open(source) as f:
-        i = Interpreter(required_words_in_varnames, legacy_vars)
-        parser = Lark(grammar)
+        grammar_file = importlib.import_module(grammar_file)
+        i = Interpreter(grammar_file.required_words_in_varnames, legacy_vars)
+        parser = Lark(grammar_file.grammar)
         for expr in parser.parse(f.read()).children:
             i.run(expr)
 

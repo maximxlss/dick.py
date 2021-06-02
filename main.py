9ae82f3837d7036708028e4aa5da48690ba88982
Maxim Kuznetsov <maximxlssoff@gmail.com>
2021-06-02 13:34:01 +0300

    now opens source in UTF-8

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from lark import Lark
 import importlib
 
 def main(source: "Path to the source code file to execute", grammar_file: "Custom grammar file without .py"="grammar", legacy_vars: "Disable LongDick varible name restrictions"=False):
-    with open(source) as f:
+    with open(source, encoding="UTF-8") as f:
         grammar_file = importlib.import_module(grammar_file)
         i = Interpreter(grammar_file.required_words_in_varnames, legacy_vars)
         parser = Lark(grammar_file.grammar)
